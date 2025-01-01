adb9a9ba331496ffded3728c6b2ac0c30030bdb0
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2025-01-01 11:40:16 -0500

    Remove blank line

--- src/test/java/com/coralblocks/coralds/ArrayBagTest.java
+++ src/test/java/com/coralblocks/coralds/ArrayBagTest.java
@@ -13,7 +13,6 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
 package com.coralblocks.coralds;
 
 import static org.junit.Assert.*;
